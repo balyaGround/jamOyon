6a453230e4b04f2dd725548efb37dec511446f72
balyazulfikar <balyazulfikar@gmail.com>
2024-02-21 01:26:11 +0700

    Login page done

--- jamoyon/src/components/form.js
+++ jamoyon/src/components/form.js
@@ -3,12 +3,11 @@ import React from "react";
 function Form() {
   return (
     <div className="bg-white px-10 py-20 rounded-3xl border-2 border-gray-200 ">
-      <h1 className="text-5xl text-yellow-600 font-semibold text-center">Welcome</h1>
-      <h1 className="text-5xl text-yellow-600 font-semibold text-center"> to </h1>
       <h1 className="text-5xl text-yellow-600 font-semibold text-center"> JamOyon Store</h1>
+      <h1 className="text-5xl text-yellow-600 font-semibold text-center mt-1">Welcome</h1>
       <p className="text-lg text-yellow-800 font-medium mt-4">please enter your Username & Password </p>
 
-      <div className="mt-8 ">
+      <div className="mt-6 ">
         <div>
           <label className="text-lg font-medium text-yellow-600">Username</label>
           <input className="w-full border-2 border-gray-200 rounded-xl p-4 mt-1 bg-transparent" placeholder="Enter Your Username" />
